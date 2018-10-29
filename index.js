e3efcee5988312765ef1453e2d0be8590d39fb5b
Spike Burton <ralphburton3@gmail.com>
2018-10-29 02:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(sel) {
+  return document.querySelector(sel);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll("ul.ranked-list li");
+
+  for(let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var cur = document.querySelectorAll('#grand-node div');
+  var next = [];
+
+  for(let i = 0; i < cur.length; i++) {
+    if(!cur[0].hasChildNodes()) {
+      return cur[i];
+    }
+  }
+}
