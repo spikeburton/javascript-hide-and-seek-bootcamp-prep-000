7fff904ece8ed69ee65cadeae999a81e3afe1097
Spike Burton <ralphburton3@gmail.com>
2018-10-29 03:22:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(sel) {
+  return document.querySelector(sel);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll("ul.ranked-list li");
+
+  for(let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var cur = document.querySelector('#grand-node');
+  var next = cur.children[0];
+
+  while(next) {
+    cur = next;
+    next = cur.children[0];
+  }
+  return cur;
+}
